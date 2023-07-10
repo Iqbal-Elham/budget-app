722dbea5e807074d888a411a5329d38c188a24e1
iqbal-elham <iqbal.elham7@gmail.com>
2023-07-10 20:42:46 +0430

    Add categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,45 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+
+  # GET /categories or /categories.json
+
+  def index
+    @categories = Category.where(user: current_user)
+  end
+
+  # GET /categories/1 or /categories/1.json
+  def show
+  end
+
+  # GET /categories/new
+  def new
+    @category = Category.new
+  end
+
+  # POST /categories or /categories.json
+  def create
+    @category = Category.new(category_params)
+    @category.user = current_user
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_path, notice: "Category was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def category_params
+      params.require(:category).permit(:name, :icon, :user_id)
+    end
+end
